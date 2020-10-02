e008c5eccf07086624a9458228faf43d160fd31f
jeremyn-nmq <nmq@netcompany.com>
2020-10-02 16:58:37 +0700

    update

--- index.js
+++ index.js
@@ -21,10 +21,30 @@ $(document).ready(function () {
       .done(function (data) {
         console.log('Success');
         console.log(data);
+        $('#token').val(data.access_token);
       })
       .fail(function (data) {
         console.log('Failed');
         console.log(data);
       });
   });
+
+  var firebaseConfig = {
+    apiKey: 'AIzaSyC_SwM-Rc9X7DHmGkz34hN18Asb6MCK2P8',
+    authDomain: 'auctionservicedev.firebaseapp.com',
+    databaseURL: 'https://auctionservicedev.firebaseio.com',
+    projectId: 'auctionservicedev',
+    storageBucket: 'auctionservicedev.appspot.com',
+    messagingSenderId: '1052773579869',
+    appId: '1:1052773579869:web:e89839c5375dfa03f5a1b4',
+    measurementId: 'G-LPDKK5YSHM',
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+  db.collection('cities')
+    .doc('test-realtime')
+    .onSnapshot(function (doc) {
+      console.log('Current data: ', doc.data());
+    });
 });
